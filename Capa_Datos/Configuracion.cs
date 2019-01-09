c35b1a1a2b8f887cf91db6db21943fe507736c95
Angel-Alvarez <angelalvarezguzman@gmail.com>
2019-01-08 19:49:14 -0600

    corrección de conexión a base de datos.

---
+++ Capa_Datos/Configuracion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capa_Datos
+{
+    class Configuracion
+    {
+        public static String G_SERVER = "localhost";
+        public static String G_PUERTO = "3307";
+        public static String G_DATABASE = "asesorias";
+        public static String G_USER_ID = "root";
+        public static String G_USER_PWD = "root";
+    }
+}
---
+++ Capa_Datos/cone.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using MySql.Web;
+using System.Data;
+
+namespace Capa_Datos
+{
+    class cone
+    {
+        public MySqlConnection conectar = new MySqlConnection();
+        public void conexion()
+        {
+            try
+            {
+                conectar.ConnectionString = "server='" + Configuracion.G_SERVER + "'; port='" + Configuracion.G_PUERTO + "' ; Database='" + Configuracion.G_DATABASE + "'; user id='" + Configuracion.G_USER_ID + "';   password='" + Configuracion.G_USER_PWD + "';  pooling=false";
+                conectar.Open();
+            }
+            catch (Exception ExceptionErr)
+            {
+                throw new System.Exception(ExceptionErr.Message, ExceptionErr.InnerException);
+            }
+        }
+    }
+}
